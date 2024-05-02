285be79d20e64be235197b231045f1187d3d0d69
Grid-Andre <andre.persad1@my.uwi.edu>
2024-05-02 17:58:16 -0400

    Content

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,10 +5,10 @@ function Body(){
     return (
         <div className='App-Body'>
             <div>
-                <h1 className='heading'>StudioFX</h1>
+                <h1 className='heading'>Avoid The Ordinary</h1>
             </div>
             <a href='index.html'>Make an Appointment</a>
-            <a href='https://www.google.com/maps/place/Studio+FX/@10.6458859,-61.4018745,17z/data=!3m1!4b1!4m6!3m5!1s0x8c3600bd2269b45d:0x1e05f45bf5230084!8m2!3d10.6458859!4d-61.3992942!16s%2Fg%2F11ggndpkwd?entry=ttu' className='location'>Location</a>
+            <a href='https://www.google.com/maps/place/Studio+FX/@10.6458859,-61.4018745,17z/data=!3m1!4b1!4m6!3m5!1s0x8c3600bd2269b45d:0x1e05f45bf5230084!8m2!3d10.6458859!4d-61.3992942!16s%2Fg%2F11ggndpkwd?entry=ttu' className='location' target="_blank" rel="noopener noreferrer">Location</a>
         </div>
     )
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,18 @@
+import './Footer.css'
 
 function Footer(){
     return (
-        <div>
-            <h2>Footer</h2>
-            <ul>
-                <li>Location</li>
+        <div className='App-Footer'>
+            <div className='Location'>
+                <a href='https://www.google.com/maps/place/Studio+FX/@10.6458859,-61.4018745,17z/data=!3m1!4b1!4m6!3m5!1s0x8c3600bd2269b45d:0x1e05f45bf5230084!8m2!3d10.6458859!4d-61.3992942!16s%2Fg%2F11ggndpkwd?entry=ttu'>32 EMR St Augustine Campus House</a>
+            </div>
+            <div className='Contact'>
+                <p>Test</p>
+            </div>
+            <div className='Follow'>
+                <p>Test</p>
 
-            </ul>
-            <ul>
-                <li>Contact</li>
-                
-            </ul>
-            <ul>
-                <li>Follow</li>
-            </ul>
+            </div>
         </div>
     )
 }
