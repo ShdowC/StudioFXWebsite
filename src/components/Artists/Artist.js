ed09f38e8d43a1f3597b57a57e70ce80791af307
Grid-Andre <andre.persad1@my.uwi.edu>
2024-05-09 17:18:49 -0400

    Main webpage adjustments

--- src/components/Artists/Artist.js
+++ src/components/Artists/Artist.js
@@ -6,15 +6,15 @@ function Artists(){
             <img src={require('../../resources/Background/ato.png')} alt='bg_img' className='bg_img'/> 
             <div className='artists'>
                 <div className="card1">
-                    <img src={require('../../resources/headshot.jpg')} alt="Artist 1" className='img1'/>
-                    <h2>Artist 1</h2>
+                    <img src={require('../../resources/Shane.png')} alt="Artist 1" className='img1'/>
+                    <h2>Shane Singh</h2>
                     <button>View Gallery</button>
                     <p>Specializes in illustrative color
                     and decorative fine line ornamental art.</p>
                 </div>
                 <div className="card2">
-                    <img src={require('../../resources/headshot.jpg')} alt="Artist 2" className='img2'/>
-                    <h2>Artist 1</h2>
+                    <img src={require('../../resources/Ramon.png')} alt="Artist 2" className='img1'/>
+                    <h2>Ramon Gonzales</h2>
                     <button>View Gallery</button>
                     <p>Specializes in illustrative color 
                     and decorative fine line ornamental art.</p>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,21 +2,9 @@ import './Footer.css'
 
 function Footer(){
     return (
-        <div className='App-Footer'>
-            <div className='Location'>
-                <a href='https://www.google.com/maps/place/Studio+FX/@10.6458859,-61.4018745,17z/data=!3m1!4b1!4m6!3m5!1s0x8c3600bd2269b45d:0x1e05f45bf5230084!8m2!3d10.6458859!4d-61.3992942!16s%2Fg%2F11ggndpkwd?entry=ttu'>32 EMR St Augustine Campus House</a>
-            </div>
-            <div className='Contact'>
-                <p>Contact</p>
-                <p>1-(868)-367-5908</p>
-                <p>1-(868)-305-9588</p>
-            </div>
+        <footer className='App-Footer'>
             <div className='Follow'>
-                <div className='artist1'>
-                    <a href='https://www.facebook.com/vs.studiofx/' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-07.png')} alt='logo' className='icon'/></a>
-                    <a href='https://www.instagram.com/studiofx.ato/?hl=en' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-03.png')} alt='logo' className='icon'/></a>
-                    <a href='https://www.tiktok.com/@studiofx868.ato?lang=en' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-01.png')} alt='logo' className='icon'/></a>
-                </div>
+                
                 <div className='artist2'>
                     <a href='https://www.facebook.com/vs.studiofx/' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-07.png')} alt='logo' className='icon'/></a>
                     <a href='https://www.instagram.com/studiofx.ato/?hl=en' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-03.png')} alt='logo' className='icon'/></a>
@@ -25,7 +13,7 @@ function Footer(){
                 
 
             </div>
-        </div>
+        </footer>
     )
 }
 
