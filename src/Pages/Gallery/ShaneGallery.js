676243149bc9dc8f2e747773c2e4014a91011793
Grid-Andre <andre.persad1@my.uwi.edu>
2024-05-13 16:45:34 -0400

    Gallery Function fixes

--- src/Pages/Gallery/ShaneGallery.js
+++ src/Pages/Gallery/ShaneGallery.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import './Gallery';
 
-function ShaneGallery() {
-   
-const feed = getStaticProps();
-console.log(feed.data);
-const images = feed.data;
+export const getStaticProps = async () => {
+    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.REACT_APP_INSTAGRAM_KEY}`;
+    const data = await fetch(url)
+    const feed = await data.json();
+    console.log(feed);
+
+    return {props: {dataFeed: feed} };
+}; 
+
+
+function ShaneGallery({dataFeed}) {
 
 return (
     <>
         <div className='gallery-container'> 
             <h1>Images</h1>
-            <div> 
-                {images && 
-                    images.map((image) => (
-                        <div key={image.id}>
-                            <img src={image.media_url} alt={image.caption} />
-                        </div>
+            <div>
+                {dataFeed?.map((image) => (
+                    <div key={image.id}>
+                        <img src={image.media_url} alt={image.caption} />
+                    </div>
+                    
                 ))}
             </div>
            
@@ -28,17 +34,5 @@ return (
 
 export default ShaneGallery;
 
-export const getStaticProps = async () => {
-    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.REACT_APP_INSTAGRAM_KEY}`;
-    const data = await fetch(url)
-    const feed = await data.json();
 
-    console.log(feed);
-
-    return {
-        props: {
-            feed,
-        },
-    };
-};
 
