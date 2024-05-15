c92190457591d3acbcefd0ddd952a49ddcf5e3ce
Grid-Andre <andre.persad1@my.uwi.edu>
2024-05-14 23:34:19 -0400

    Footer Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,14 @@ function Footer(){
     return (
         <footer className='App-Footer'>
             <div className='Follow'>
-                
                 <div className='artist2'>
                     <a href='https://www.facebook.com/vs.studiofx/' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-07.png')} alt='logo' className='icon'/></a>
                     <a href='https://www.instagram.com/studiofx.ato/?hl=en' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-03.png')} alt='logo' className='icon'/></a>
                     <a href='https://www.tiktok.com/@studiofx868.ato?lang=en' target="_blank" rel="noopener noreferrer"><img src={require('../../resources/icons/red-01.png')} alt='logo' className='icon'/></a>
                 </div>
-                
-
+            </div>
+            <div className='Company_'>
+                    <span>© 2024 ShadowSoftware. All Rights Reserved</span>
             </div>
         </footer>
     )
